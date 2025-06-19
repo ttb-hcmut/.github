79b8758c9e5b258ce21b811b6a86bcbfef77717e
Kinten Le <kinten108101@protonmail.com>
2025-06-19 10:53:43 +0700

    microcluster_exec: flattened lib hierarchy

--- microcluster_exec/lib/lib.ml
+++ microcluster_exec/lib/lib.ml
@@ -1,7 +1,3 @@
-module Script = Script
-module Command = Command
-module Language = Language
-
 let language_of_runner : Command.runner -> Language.t = function
   | `Python -> `LanguagePython
   | `Utop   -> `LanguageOCaml
